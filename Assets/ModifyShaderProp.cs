1007d9f93b382219890220002ef7c72bd3fedff0
Mangirish Naik <naikmangirish@gmail.com>
2018-06-19 20:53:19 +0800

    First Commit

---
+++ Assets/ModifyShaderProp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyShaderProp : MonoBehaviour {
+    public float Stencil_Comparison = 8;
+    public float Stencil_ID = 0;
+    public float Stencil_Operation = 0;
+    public float Stencil_Write_Mask = 255;
+    public float Stencil_Read_Mask = 255;
+    
+	// Use this for initialization
+	void Start () {
+        Material m = transform.GetComponent<MeshRenderer>().material;
+        m.SetFloat("Stencil Comparison", Stencil_Comparison);
+        m.SetFloat("Stencil ID", Stencil_ID);
+        m.SetFloat("Stencil Operation", Stencil_Operation);
+        m.SetFloat("Stencil Write Mask", Stencil_Write_Mask);
+        m.SetFloat("Stencil Read Mask", Stencil_Read_Mask);
+
+
+        //_StencilComp("", Float) = 8
+        //_Stencil("", Float) = 0
+        //_StencilOp("", Float) = 0
+        //_StencilWriteMask("Stencil Write Mask", Float) = 255
+        //_StencilReadMask("Stencil Read Mask", Float) = 255
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UI3DScroll.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+// 3D Masking Original Script from:  https://forum.unity3d.com/threads/ui-and-masking-3d-meshes-in-a-scrollrect.358475/
+[ExecuteInEditMode]
+public class UI3DScroll : MonoBehaviour {
+
+    public List<MeshFilter> meshFilters = new List<MeshFilter>();
+
+    void Start()
+    {
+        if (meshFilters.Count > 0)
+            meshFilters.Clear();
+        meshFilters.AddRange(transform.GetComponentsInChildren<MeshFilter>());
+        
+    }
+
+    void ResetData()
+    {
+        meshFilters.ForEach(delegate (MeshFilter meshFilter)
+        {
+            var cr = meshFilter.transform.GetComponent<CanvasRenderer>();
+            cr.SetMesh(meshFilter.sharedMesh);
+
+            List<Material> materials = meshFilter.transform.GetComponent<Renderer>().sharedMaterials.ToList();
+
+            for (int i = 0; i < materials.Count; i++)
+            {
+                cr.materialCount = materials.Count;
+                cr.SetMaterial(materials[i], i);
+            }
+        });
+    }
+
+    void OnEnable()
+    {
+        ResetData();
+    }
+
+    void OnValidate()
+    {
+        ResetData();
+    }
+}
